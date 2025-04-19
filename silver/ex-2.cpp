0da4b1ac160032d3009d4401fad7819f0b5ac9a1
adaniel105 <dakinola18@gmail.com>
2025-04-19 23:46:17 +0100

    mod

--- silver/ex-2.cpp
+++ silver/ex-2.cpp
@@ -317,6 +317,56 @@ int main(){
    
 }
 
+//https://usaco.org/index.php?page=viewproblem2&cpid=858
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int N,M,C;
+int t[100000];
+ 
+//montonic fn implementation
+bool pos(int wait)
+{
+	int wagons = 1;
+	int firstArrival = t[0];
+	int firstIndex = 0;
+	for(int i=1;i<N;i++)
+	{
+        //bus takes either the first C cows, or all cows that have arrived T timesteps after the first
+		if(t[i] - firstArrival > wait || i + 1 - firstIndex > C)
+		{
+			wagons += 1;
+			firstArrival = t[i];
+			firstIndex = i;
+		}
+	}
+	return (wagons <= M);
+}
+ 
+int binSearch(int low,int high)
+{
+	if(low == high) return low;
+	if(low + 1 == high)
+	{
+		if(pos(low)) return low;
+		return high;
+	}
+	int mid = (low+high)/2;
+	if(pos(mid)) return binSearch(low,mid);
+	else return binSearch(mid+1,high);
+}
+ 
+int main()
+{
+	cin >> N >> M >> C;
+	for(int i=0;i<N;i++)
+		cin >> t[i];
+	sort(t,t+N);
+	cout << binSearch(0, 1000000000) << '\n';
+}
+
+
 
 
 
