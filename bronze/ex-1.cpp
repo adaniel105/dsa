8fa02199f16b479b6cf22d1f4926e6e23cea1f25
adaniel105 <dakinola18@gmail.com>
2026-02-13 23:53:04 +0100

    update bronze.
    Added logic to find the first number with all unique digits.

--- bronze/ex-1.cpp
+++ bronze/ex-1.cpp
@@ -1,5 +1,33 @@
 //<1300 PROBLEMS //
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int n, ncopy;
+    int arr[4];
+    cin >> n;
+
+    while(true){
+        n++;
+        int a = n / 1000;
+        int b = n / 100 % 10;
+        int c = n / 10 % 10;
+        int d = n % 10;
+
+        if(a != b && a != c && a !=d && b != c && b != d && c != d){
+            break;
+        } 
+    }
+
+    cout << n;
+}
+
+
+
+
+
 //https://codeforces.com/problemset/problem/266/B (incomplete)
 #include <bits/stdc++.h>
 using namespace std;
